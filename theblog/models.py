05ba24756eaaaabd770188cb7eec7299b5cb505f
OmerEkinci1 <omrekinci5@gmail.com>
2020-07-16 00:48:45 +0300

    Add a create profile page and modified edit profile page

--- theblog/models.py
+++ theblog/models.py
@@ -24,10 +24,12 @@ class Profile(models.Model):
     instagram_url = models.CharField(max_length= 255,null=True, blank=True)
     linkedn_url = models.CharField(max_length= 255,null=True, blank=True)
 
-
-
     def __str__(self):
         return str(self.user)
+
+    def get_absolute_url(self):
+        # return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
 
 class Post(models.Model):
     title = models.CharField(max_length= 255)
